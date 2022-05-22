d38ec4e55989aed220b7e874000006d7c02bc174
Rodrigo Emídio <94897828+RodrigoDoGit@users.noreply.github.com>
2022-05-22 13:55:12 +0100

    Resolução do primeiro exercício da Folha #1

--- java_intro/Numbers.java
+++ java_intro/Numbers.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+// Uma classe muito simples que usa um ciclo para imprimir numeros entre 1 e n
+
+// O ficheiro tem de ter o mesmo nome da classe + a extensão '.java'
+public class Numbers {
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int soma = 0; 
+	
+	System.out.println("Numeros de 1 a " + n + ":"); // + é o operador de concatenação de strings
+	for(int i=1; i<=n; i++){
+	    soma += i;
+
+	    while(i<=n-1){
+		System.out.print(i + ",");
+		i++;
+		soma += i;
+	    }
+
+	    System.out.println(i);
+	}
+
+	System.out.println("Soma dos numeros de 1 a " + n + ": " + soma);
+    }
+}
